1c543788766dd807c23a12879e4dc8deb162130c
Olivier <o.lespagnon@gmail.com>
2018-11-26 10:38:07 +0100

    Avatar page profil

--- view/profilePage.php
+++ view/profilePage.php
@@ -1,7 +1,7 @@
 <?php
     require('template/top.php');
     require('template/navbar.php');
-    echo('<div class="card" style="width: 18rem;"><div class="card-header">Profil</div><ul class="list-group list-group-flush">');
+    echo('<div class="card" style="width: 18rem;"><div class="card-header text-center">Profil</div><div class="card-avatar"><center><img src="public/images/avatar/'.$perso_data_arr['avatar'].'" height="150px;"></center><ul class="list-group list-group-flush">');
     foreach ($perso_data_arr as $key => $value){
         echo('<li class="list-group-item">'.$key.' : '.$value.'</li>');
     }
