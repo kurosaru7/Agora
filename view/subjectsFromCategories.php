fb4ada0471d079b822665972bd16a9dd69d54bd4
Olivier <o.lespagnon@gmail.com>
2018-11-27 14:19:20 +0100

    Affihage des sujets par catégories

--- view/subjectsFromCategories.php
+++ view/subjectsFromCategories.php
@@ -10,20 +10,11 @@
 </thead>
 <tbody class="table-hover">
 <?php
-for ($i=0; $i < count($subjects); $i++) { 
-    foreach ($subjects[$i] as $key => $value) {
-        $pseudo = getUserById($subjects[$i]['profil']);
-        $dateHeure = explode(" ",$subjects[$i]['dateS']);
+foreach ($subjects as $key => $value) {
+        $pseudo = getUserById($value['profil']);
+        $dateHeure = explode(" ",$value['dateS']);
         echo '<tr>
-                <td class="text-left"><a href = index.php?action=printSubject&id='.$subjects[$i]['id'].'>'.$subjects[$i]['nom'].'</a><font color="purple"> | Créateur </font>: '.$pseudo['pseudo'].'<font color="purple"> | Crée le : </font>'.$dateHeure[0].' <font color="purple">à</font> '. $dateHeure[1].'</td>
+                <td class="text-left"><a href = index.php?action=printSubject&id='.$value['id'].'>'.$value['nom'].'</a><font color="purple"> | Créateur </font>: '.$pseudo['pseudo'].'<font color="purple"> | Crée le : </font>'.$dateHeure[0].' <font color="purple">à</font> '. $dateHeure[1].'</td>
             </tr>';
-    }
-    // print_r($subjects[$i]);
-    // echo('<br>');
 }
-// foreach ($subjects as $key => $value) {
-//     echo '<tr>
-//     <td class="text-left"><a href = index.php?action=printSubject&id='.$idSujet[$i][$y].'>'.$nomSujet[$i][$y].'</a><font color="purple"> | Créateur </font>: '.$pseudo[$i][$y].'<font color="purple"> | Créer le : </font>'.$dateHeure[$i][$y][0].' <font color="purple">à</font> '. $dateHeure[$i][$y][1].'</td>
-//     </tr>';
-// }
 echo '</tbody></table>';
